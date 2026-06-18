3565705cbbb7efd721bf0df479b8980bfc42766b
Nbioni <43020119+Nbioni@users.noreply.github.com>
2026-06-18 19:22:15 -0300

    Ajustes de performance

--- next.config.ts
+++ next.config.ts
@@ -18,12 +18,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 375, 425, 520, 640, 750, 828, 1080, 1200, 1350, 1400, 1500, 1600, 1920, 2048, 3840],
   },
