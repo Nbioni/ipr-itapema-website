f0fb507c8e33370f0fa8ad2e83d1211ac3419367
Nbioni <43020119+Nbioni@users.noreply.github.com>
2026-06-18 18:29:51 -0300

    garantias de seguranca

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://images.unsplash.com;
+    font-src 'self' data:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    frame-src 'self' https://maps.google.com https://www.youtube.com;
+    upgrade-insecure-requests;
+`.replace(/\s{2,}/g, ' ').trim();
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
@@ -12,6 +26,39 @@ const nextConfig: NextConfig = {
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 375, 425, 520, 640, 750, 828, 1080, 1200, 1350, 1400, 1500, 1600, 1920, 2048, 3840],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
   },
 };
 
