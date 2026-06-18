b0ec2738502a4297f982a680134195deca42d25e
Nbioni <43020119+Nbioni@users.noreply.github.com>
2026-06-18 19:26:04 -0300

    Ajustes na segurança

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,13 @@ const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://images.unsplash.com;
+    img-src 'self' blob: data:;
     font-src 'self' data:;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    frame-src 'self' https://maps.google.com https://www.youtube.com;
+    frame-src 'self' https://maps.google.com https://www.google.com https://www.youtube.com;
     upgrade-insecure-requests;
 `.replace(/\s{2,}/g, ' ').trim();
 
