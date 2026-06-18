431de54a69733073d17432ab84d4c8872f906556
Nbioni <43020119+Nbioni@users.noreply.github.com>
2026-06-18 19:37:32 -0300

    melhoria de performance

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const cspHeader = `
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    // Força o Next.js a otimizar a importação de pacotes de terceiros pesados
+    optimizePackageImports: [
+      'lucide-react', 
+    ],
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 375, 425, 520, 640, 750, 828, 1080, 1200, 1350, 1400, 1500, 1600, 1920, 2048, 3840],
